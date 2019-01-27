1d6696c3d3a594789f5dd8d381459f052cbc4d99
mojo1643 <johnrejimathai@gmail.com>
2019-01-26 22:53:37 -0600

    only key phrases

--- api/cognitiveApi.js
+++ api/cognitiveApi.js
@@ -1,11 +1,146 @@
 const fs = require('fs');
 const request = require('request');
+let https = require ('https');
 
 // Replace <Subscription Key> with your valid subscription key.
 const subscriptionKey = '8d638ec7c9734f5e8196aace92c8bfa2', 
       uriBase = 'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr';
 
+//if success proceed to text analytics
+// text analytics starts here ---------------------------------------------------------------------------------------------------------------------
+
+// Replace the accessKey string value with your valid access key.
+let accessKey = '194b6f78567c46d4847677737b54c25d';
+
+// Replace or verify the region.
+
+// You must use the same region in your REST API call as you used to obtain your access keys.
+// For example, if you obtained your access keys from the westus region, replace 
+// "westcentralus" in the URI below with "westus".
+
+// NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+// a free trial access key, you should not need to change this region.
+let uri = 'westcentralus.api.cognitive.microsoft.com';
+let path = '/text/analytics/v2.0/keyPhrases';
+
+
+let SUBSCRIPTION_WEBSEARCH_KEY = '1765443c4eb3423dad32655c6fe7250b';
+
 module.exports = {
+    // bingsearch: function(req, res, query){
+    //     https.get({
+    //         hostname: 'api.cognitive.microsoft.com',
+    //         path:     '/bing/v7.0/search?q=' + encodeURIComponent(query),
+    //         headers:  { 'Ocp-Apim-Subscription-Key': SUBSCRIPTION_WEBSEARCH_KEY },
+    //       }, res => {
+    //         let body = ''
+    //         res.on('data', part => body += part)
+    //         res.on('end', () => {
+    //           for (var header in res.headers) {
+    //             if (header.startsWith("bingapis-") || header.startsWith("x-msedge-")) {
+    //               console.log(header + ": " + res.headers[header])
+    //             }
+    //           }
+    //           console.log('\nJSON Response:\n')
+    //           console.dir(JSON.parse(body), { colors: false, depth: null })
+    //         })
+    //         res.on('error', e => {
+    //           console.log('Error: ' + e.message)
+    //           throw e
+    //         })
+    //       })
+    // },
+    // bingsearchloop: function(req, res, str_arr){
+    //     var urls = ['http://adrianmejia.com/atom.xml', 'http://twitrss.me/twitter_user_to_rss/?user=amejiarosario'];
+    //     var completed_requests = 0;
+
+    //     str_arr.forEach(function(str_arr) {
+
+    //     var responses = [];
+
+    //     http.get(url, function(res) {
+    //         res.on('data', function(chunk){
+    //         responses.push(chunk);
+    //         });
+
+    //         res.on('end', function(){
+    //         if (completed_requests++ == urls.length - 1) {
+    //             // All downloads are completed
+    //             console.log('body:', responses.join());
+    //         }      
+    //         });
+    //     });
+
+    //     https.get({
+    //         hostname: 'api.cognitive.microsoft.com',
+    //         path:     '/bing/v7.0/search?q=' + encodeURIComponent(query),
+    //         headers:  { 'Ocp-Apim-Subscription-Key': SUBSCRIPTION_WEBSEARCH_KEY },
+    //       }, res => {
+    //         let body = ''
+    //         res.on('data', part => body += part)
+    //         res.on('end', () => {
+    //           for (var header in res.headers) {
+    //             if (header.startsWith("bingapis-") || header.startsWith("x-msedge-")) {
+    //               console.log(header + ": " + res.headers[header])
+    //             }
+    //           }
+    //           console.log('\nJSON Response:\n')
+    //           console.dir(JSON.parse(body), { colors: false, depth: null })
+    //         })
+    //         res.on('error', e => {
+    //           console.log('Error: ' + e.message)
+    //           throw e
+    //         })
+    //       })
+
+
+    //     })
+    // },
+    key_phrase: function(req, res, str){
+        let response_handler = function (response) {
+            let body = '';
+            response.on ('data', function (d) {
+                body += d;
+            });
+            response.on ('end', function () {
+                let body_ = JSON.parse (body);
+                let body__ = JSON.stringify (body_, null, '  ');
+                
+                //return success here
+                body__ = JSON.parse (body__);
+                console.log (body__);
+                console.log(body__['documents'][0]['keyPhrases']);
+                res.send(body__['documents'][0]['keyPhrases']);
+            });
+            response.on ('error', function (e) {
+                console.log ('Error: ' + e.message);
+            });
+        };
+  
+        let get_key_phrases = function (documents) {
+            let body = JSON.stringify (documents);
+  
+            let request_params = {
+                method : 'POST',
+                hostname : uri,
+                path : path,
+                headers : {
+                    'Ocp-Apim-Subscription-Key' : accessKey,
+                }
+            };
+  
+            let req = https.request (request_params, response_handler);
+            req.write (body);
+            req.end ();
+        }
+  
+        let documents = { 'documents': [
+            { 'id': '1', 'language': 'en', 'text': str+'' },
+        ]};
+  
+        get_key_phrases (documents);
+      },
+
     ocr: function(req, res) {
         var tmp_path = req.file.path;
         var target_path = './images/' + req.file.originalname;
@@ -40,7 +175,7 @@ module.exports = {
             let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
             
             jsonResponse = JSON.parse(jsonResponse)
-            console.log('JSON Response: ' + jsonResponse +'\n');
+            // console.log('JSON Response: ' + jsonResponse +'\n');
             try {
               for(var i = 0; i < jsonResponse.regions[0].lines.length; i++)
               {
@@ -48,16 +183,19 @@ module.exports = {
                   for(var j = 0; j < words.length; j++)
                   {
                     concatenate_str += words[j].text+' ';
-                     console.log("text: " + words[j].text);
+                    // console.log("text: " + words[j].text);
                   }
               }
+
             }
             catch(e){
               res.send("Invalid Url.");
               return;
             }
-    
-            res.sendStatus(200);
+            // console.log(concatenate_str);
+
+            module.exports.key_phrase(req,res,concatenate_str);
+            // res.sendStatus(200);
           });
         });
     
